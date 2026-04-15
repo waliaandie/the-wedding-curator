beba6dc8e98679511b32827939654e88ea5b6c5e
waliaandie <walia.andie@gmail.com>
2026-04-14 20:58:59 -0400

    Delete main.jsx

--- main.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import WeddingCuratorLandingPage from './WeddingCuratorLandingPage.jsx'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <WeddingCuratorLandingPage />
-  </React.StrictMode>,
-)
